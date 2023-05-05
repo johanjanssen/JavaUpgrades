ba0e73b4e8468dd0c426033d5d256667a31def5b
Johan Janssen <johanjanssen2001@gmail.com>
2023-05-05 21:36:18 +0200

    Updated multi-release-jar example to use the latest (minor) Java versions.

--- multi-release-jar/run-multi-release-application.cmd
+++ multi-release-jar/run-multi-release-application.cmd
@@ -1,4 +1,4 @@
-docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target eclipse-temurin:8u362-b09-jre-alpine java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
-docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target eclipse-temurin:11.0.18_10-jre-alpine java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
-docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target eclipse-temurin:17.0.6_10-jre-alpine java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
-docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target eclipse-temurin:19.0.2_7-jre-alpine java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
+docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target eclipse-temurin:8u372-b07-jre-alpine java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
+docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target eclipse-temurin:11.0.19_7-jre-alpine java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
+docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target eclipse-temurin:17.0.7_7-jre-alpine java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
+docker run -v /c/Work/JavaUpgrades/multi-release-jar/target:/target openjdk:21-slim java -jar /target/multi-release-jar-1.0-SNAPSHOT.jar
